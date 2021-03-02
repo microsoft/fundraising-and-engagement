29a651c55ff7b5807df34f869c8665dee312567e
jmichelfeit <jmichelfeit@microsoft.com>
2021-03-02 12:06:24 +0100

    Update generate-padlock.ps1

--- CDS.Solutions.Nonprofit/FundraisingandEngagementServices/generate-padlock.ps1
+++ CDS.Solutions.Nonprofit/FundraisingandEngagementServices/generate-padlock.ps1
@@ -1,14 +1,17 @@
-﻿
-Add-Type -AssemblyName 'System.Web'
-$password = [System.Web.Security.Membership]::GeneratePassword(30,3)
-$passwordBytes = [System.Text.Encoding]::Unicode.GetBytes($password)
-$tdes = New-Object System.Security.Cryptography.TripleDESCryptoServiceProvider
-$pdb = New-Object System.Security.Cryptography.PasswordDeriveBytes @($passwordBytes, @())
-$key = $pdb.CryptDeriveKey("TripleDES", "SHA1", 192, $tdes.IV);
-$hashedPassword = [Convert]::ToBase64String($key)
+function Generate-Padlock-Token {
+    Add-Type -AssemblyName 'System.Web'
+    $password = [System.Web.Security.Membership]::GeneratePassword(30,3)
+    $passwordBytes = [System.Text.Encoding]::Unicode.GetBytes($password)
+    $tdes = New-Object System.Security.Cryptography.TripleDESCryptoServiceProvider
+    $pdb = New-Object System.Security.Cryptography.PasswordDeriveBytes @($passwordBytes, @())
+    $key = $pdb.CryptDeriveKey("TripleDES", "SHA1", 192, $tdes.IV);
+    $hashedPassword = [Convert]::ToBase64String($key)
 
-Write-Host "Unencrypted padlock token (goes to Configuration record in F&E application): "
-Write-Host -ForegroundColor Green $password
+    Write-Host "`n`nUnencrypted padlock token (goes to Configuration record in F&E application): "
+    Write-Host -ForegroundColor Green $password
 
-Write-Host "`nHashed padlock token (goes to Azure ARM template parameters): "
-Write-Host -ForegroundColor Green $hashedPassword
+    Write-Host "`nHashed padlock token (goes to Azure ARM template parameters): "
+    Write-Host -ForegroundColor Green "$hashedPassword`n"
+}
+
+Generate-Padlock-Token
